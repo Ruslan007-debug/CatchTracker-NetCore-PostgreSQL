51ced59fbc443538a0eeaf23dac982495404e81b
Ruslan Terpak <ruxa.terpak@gmail.com>
2026-01-23 20:09:08 +0200

    added FishType DTOs and dotEnv file

---
+++ CatchTrackerApi/CatchTrackerApi/DTOs/FishTypeDTOs/CreateFishTypeDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CatchTrackerApi.DTOs.FishTypeDTOs
+{
+    public class CreateFishTypeDTO
+    {
+        public string TypeName { get; set; } = string.Empty;
+        public string FavBait { get; set; } = string.Empty;
+        public double AvgWeight { get; set; }
+    }
+}
---
+++ CatchTrackerApi/CatchTrackerApi/DTOs/FishTypeDTOs/FishTypeDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CatchTrackerApi.DTOs.FishTypeDTOs
+{
+    public class FishTypeDTO
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; } = string.Empty;
+        public string FavBait { get; set; }= string.Empty;
+        public double AvgWeight { get; set; }
+    }
+}
