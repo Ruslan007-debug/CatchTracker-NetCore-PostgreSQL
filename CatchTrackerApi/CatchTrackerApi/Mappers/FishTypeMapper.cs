11475da44f328d59bab1455974d12a147fbdfbf0
Ruslan Terpak <ruxa.terpak@gmail.com>
2026-01-23 20:26:28 +0200

    made FishType mapper

---
+++ CatchTrackerApi/CatchTrackerApi/Mappers/FishTypeMapper.cs
@@ -0,0 +1,40 @@
+﻿using CatchTrackerApi.DTOs.FishTypeDTOs;
+using CatchTrackerApi.Models;
+
+
+namespace CatchTrackerApi.Mappers
+{
+    public static class FishTypeMapper
+    {
+        public static FishTypeDTO? ToFishTypeDTO(this FishType fishType)
+        {
+            return new FishTypeDTO
+            {
+                Id = fishType.Id,
+                TypeName = fishType.TypeName,
+                FavBait = fishType.FavBait,
+                AvgWeight = fishType.AvgWeight
+            };
+        }
+
+        public static FishType ToFishTypeFromCreateDto(this CreateFishTypeDTO createFishType)
+        {
+            return new FishType
+            {
+                TypeName = createFishType.TypeName,
+                FavBait = createFishType.FavBait,
+                AvgWeight = createFishType.AvgWeight
+            };
+        }
+
+        public static FishType ToFishTypeFromUpdateDto(this UpdateFishTypeDTO updateFishType)
+        {
+            return new FishType
+            {
+                TypeName = updateFishType.TypeName,
+                FavBait = updateFishType.FavBait,
+                AvgWeight = updateFishType.AvgWeight
+            };
+        }
+    }
+}
