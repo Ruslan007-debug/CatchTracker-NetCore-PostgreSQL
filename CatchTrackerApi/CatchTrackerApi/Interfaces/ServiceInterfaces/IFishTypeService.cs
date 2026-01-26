737a50939833028e4902ac4035380907021853ae
Ruslan Terpak <ruxa.terpak@gmail.com>
2026-01-26 15:05:57 +0200

    added FishTypeService and IFishTypeService

---
+++ CatchTrackerApi/CatchTrackerApi/Interfaces/ServiceInterfaces/IFishTypeService.cs
@@ -0,0 +1,13 @@
+﻿using CatchTrackerApi.Models;
+
+namespace CatchTrackerApi.Interfaces.ServiceInterfaces
+{
+    public interface IFishTypeService
+    {
+        Task<List<FishType>> GetAllAsync();
+        Task<FishType?> GetByIdAsync(int id);
+        Task<FishType?> DeleteAsync(int id);
+        Task<FishType> CreateAsync(FishType fishTypeModel);
+        Task<FishType> UpdateAsync(int id, FishType fishTypeModel);
+    }
+}
