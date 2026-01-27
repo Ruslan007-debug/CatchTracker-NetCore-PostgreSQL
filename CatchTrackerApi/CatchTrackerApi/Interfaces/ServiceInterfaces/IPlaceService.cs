06fab0abf7598993c1224a93004faa2c84ecc0ec
Ruslan Terpak <ruxa.terpak@gmail.com>
2026-01-27 19:36:43 +0200

    maid PlaceService and IPlaceInterface

---
+++ CatchTrackerApi/CatchTrackerApi/Interfaces/ServiceInterfaces/IPlaceService.cs
@@ -0,0 +1,13 @@
+﻿using CatchTrackerApi.Models;
+
+namespace CatchTrackerApi.Interfaces.ServiceInterfaces
+{
+    public interface IPlaceService
+    {
+        Task<List<Place>> GetAllAsync();
+        Task<Place?> GetByIdAsync(int id);
+        Task<Place> CreateAsync(Place place);
+        Task<Place> UpdateAsync(Place place, int id);
+        Task<Place?> DeleteAsync(int id);
+    }
+}
---
+++ CatchTrackerApi/CatchTrackerApi/Services/PlaceService.cs
@@ -0,0 +1,51 @@
+﻿using CatchTrackerApi.Interfaces.ServiceInterfaces;
+using CatchTrackerApi.Interfaces.RepoInterfaces;
+using CatchTrackerApi.Models;
+
+namespace CatchTrackerApi.Services
+{
+    public class PlaceService: IPlaceService
+    {
+        public readonly IPlaceRepository _placeRepo;
+
+        public PlaceService(IPlaceRepository placeRepo)
+        {
+            _placeRepo = placeRepo;
+        }
+
+        public async Task<Place> CreateAsync(Place place)
+        {
+            return await _placeRepo.CreateAsync(place);
+        }
+
+        public async Task<Place?> DeleteAsync(int id)
+        {
+            var deleted = await _placeRepo.DeleteAsync(id);
+            if (deleted == null) 
+            {
+                throw new KeyNotFoundException($"Place with ID: {id} is not found");
+            }
+            return deleted;
+        }
+
+        public async Task<List<Place>> GetAllAsync()
+        {
+            return await _placeRepo.GetAllAsync();
+        }
+
+        public Task<Place?> GetByIdAsync(int id)
+        {
+            return _placeRepo.GetByIdAsync(id);
+        }
+
+        public async Task<Place> UpdateAsync(Place place, int id)
+        {
+            var updating = await _placeRepo.UpdateAsync(place, id);
+            if (updating == null)
+            {
+                throw new KeyNotFoundException($"Place with ID: {id} is not found");
+            }
+            return updating;
+        }
+    }
+}
