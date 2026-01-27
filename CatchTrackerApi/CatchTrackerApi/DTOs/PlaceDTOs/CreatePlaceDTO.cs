c07002f801d55d5913c68bb19f382458e8b3076c
Ruslan Terpak <ruxa.terpak@gmail.com>
2026-01-27 18:36:36 +0200

    maid Place DTOs and Mapper

---
+++ CatchTrackerApi/CatchTrackerApi/DTOs/PlaceDTOs/CreatePlaceDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CatchTrackerApi.DTOs.PlaceDTOs
+{
+    public class CreatePlaceDTO
+    {
+        public string Name { get; set; }
+        public string BiggestTrophy { get; set; }
+        public double WaterTemp { get; set; }
+    }
+}
---
+++ CatchTrackerApi/CatchTrackerApi/DTOs/PlaceDTOs/PlaceDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CatchTrackerApi.DTOs.PlaceDTOs
+{
+    public class PlaceDTO
+    {
+        public int Id { get; set; }              
+        public string Name { get; set; }
+        public string BiggestTrophy { get; set; }
+        public double WaterTemp { get; set; }
+    }
+}
