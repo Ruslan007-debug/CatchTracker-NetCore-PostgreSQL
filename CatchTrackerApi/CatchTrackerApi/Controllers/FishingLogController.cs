6cb7591c43efb64af26657efabc69dc1089c217a
Ruslan Terpak <ruxa.terpak@gmail.com>
2026-01-31 20:06:10 +0200

    add FishingLogController and registred methods and interfaces in Program.cs, changed time in  FishingLog model (UtcNow)

---
+++ CatchTrackerApi/CatchTrackerApi/Controllers/FishingLogController.cs
@@ -0,0 +1,116 @@
+﻿using CatchTrackerApi.DTOs.FishingLogDTOs;
+using CatchTrackerApi.Interfaces.ServiceInterfaces;
+using CatchTrackerApi.Mappers;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatchTrackerApi.Controllers
+{
+    [ApiController]
+    [Route("api/FishingLogs")]
+    public class FishingLogController : ControllerBase
+    {
+        private readonly IFishingLogService _fishingLogService;
+        public FishingLogController(IFishingLogService fishingLogService)
+        {
+            _fishingLogService = fishingLogService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var fishingLogs = await _fishingLogService.GetAllFishingLogsAsync();
+                var fishingLogsDto = fishingLogs.Select(f=>f.ToFishingLogDTO()).ToList();
+                return Ok(fishingLogsDto);
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult(new { message = "Сталася внутрішня помилка сервера.", details = ex.Message }) { StatusCode = 500 };
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            try 
+            {
+                var fishingLog = await _fishingLogService.GetFishingLogByIdAsync(id);
+                if (fishingLog == null)
+                {
+                    return NotFound(new { message = $"Fishing log with ID {id} not found." });
+                }
+                return Ok(fishingLog.ToFishingLogDTO());
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+        }
+
+        [HttpGet("user/{userId}")]
+        public async Task<IActionResult> GetByUserId([FromRoute] int userId)
+        {
+            try
+            {
+                var fishingLogs = await _fishingLogService.GetFishingLogsByUserIdAsync(userId);
+                var fishingLogsDto = fishingLogs.Select(f => f.ToFishingLogDTO()).ToList();
+                return Ok(fishingLogsDto);
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult(new { message = "Сталася внутрішня помилка сервера.", details = ex.Message }) { StatusCode = 500 };
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateFishingLogDTO createDto)
+        {
+            try
+            {
+                var FishingLog = createDto.ToFishingLogFromCreateDTO();
+                var createdFishingLog = await _fishingLogService.CreateFishingLogAsync(FishingLog);
+                return CreatedAtAction(nameof(GetById), new { id = createdFishingLog.Id }, createdFishingLog.ToFishingLogDTO());
+            }
+            catch (InvalidOperationException ex)
+            {
+                return Conflict(new { message = "Сталася внутрішня помилка сервера.", details = ex.Message });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromBody] UpdateFishingLogDTO updatedDTO, [FromRoute] int id)
+        {
+            var updatedFishingLog = updatedDTO.ToFishingLogFromUpdateDTO();
+            try
+            {
+                var fishingLog = await _fishingLogService.UpdateFishingLogAsync(updatedFishingLog, id);
+                return Ok(fishingLog.ToFishingLogDTO());
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Внутрішня помилка", details = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            try
+            {
+                var deleted = await _fishingLogService.DeleteFishingLogAsync(id);
+                return NoContent();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+        }
+
+    }
+}
