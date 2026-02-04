7562bfeafce1b272cee2ffb9fc1f42c2589c7280
Ruslan Terpak <ruxa.terpak@gmail.com>
2026-02-04 21:01:26 +0200

    maid UserRepository and IUserRepository

--- CatchTrackerApi/CatchTrackerApi/Interfaces/RepoInterfaces/IUserRepository.cs
+++ CatchTrackerApi/CatchTrackerApi/Interfaces/RepoInterfaces/IUserRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace CatchTrackerApi.Interfaces.RepoInterfaces
+﻿using CatchTrackerApi.Models;
+
+namespace CatchTrackerApi.Interfaces.RepoInterfaces
 {
     public interface IUserRepository
     {
+        Task<User?> GetUserByIdAsync(int id);
+        Task<User?> GetUserByEmailAsync(string email);
+        Task<User?> GetUserByUsernameAsync(string username);
+        Task<User?> GetUserByRefreshTokenAsync(string refreshToken);
+        Task<User> CreateUserAsync(User user);
+        Task<User?> UpdateUserAsync(User user);
+        Task<User?> DeleteUserAsync(int id);
     }
 }
