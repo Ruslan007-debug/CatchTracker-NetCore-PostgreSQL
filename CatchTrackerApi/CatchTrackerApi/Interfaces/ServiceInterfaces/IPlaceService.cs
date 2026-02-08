3a2fe33f81cc79aa8253eb46e143ef2a3a938e2b
Ruslan Terpak <ruxa.terpak@gmail.com>
2026-02-08 20:54:28 +0200

    added serch by name for places and fish types

--- CatchTrackerApi/CatchTrackerApi/Interfaces/ServiceInterfaces/IPlaceService.cs
+++ CatchTrackerApi/CatchTrackerApi/Interfaces/ServiceInterfaces/IPlaceService.cs
@@ -1,10 +1,11 @@
 ﻿using CatchTrackerApi.Models;
+using CatchTrackerApi.Queries;
 
 namespace CatchTrackerApi.Interfaces.ServiceInterfaces
 {
     public interface IPlaceService
     {
-        Task<List<Place>> GetAllAsync();
+        Task<List<Place>> GetAllAsync(QueryForPlace query);
         Task<Place?> GetByIdAsync(int id);
         Task<Place> CreateAsync(Place place);
         Task<Place> UpdateAsync(Place place, int id);
